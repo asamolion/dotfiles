ea30f32037dab41713856a90339287c242f5e6b4
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2018-04-10 14:46:12 +0500

    Improve: add install script and nvm/node.js installation

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install v8
+
