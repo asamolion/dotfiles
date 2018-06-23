874428c69fba8cf30c9dbad2c7f167d5ddcec9cf
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2018-06-23 23:06:54 +0500

    IMPROVE: add terminator config dir

--- symlink.sh
+++ symlink.sh
@@ -4,6 +4,9 @@
 dotfiles_dir=~/dotfiles
 
 
+# create dirs
+mkdir ~/.config/terminator/
+
 # create symlinks
 sudo ln -sf $dotfiles_dir/vimrc ~/.vimrc
 sudo ln -sf $dotfiles_dir/bash_aliases ~/.bash_aliases
