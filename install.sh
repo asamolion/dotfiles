b3973245643f5f65b76ccaa8902e0d9988970aac
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2018-12-22 11:21:14 +0500

    Sat Dec 22 11:21:14 PKT 2018

--- install.sh
+++ install.sh
@@ -2,14 +2,19 @@
 
 sudo apt-get install \
     terminator \
+    sublime-text \
+    sublime-merge \
     vim vim-gtk3 \
     curl \
     vagrant \
     virtualbox \
     apt-transport-https \
     ca-certificates \
-    curl \
-    software-properties-common
+    software-properties-common \
+    megacmd
+
+
+sudo snap install postman
 
 # Install nvm
 get_latest_release() {
@@ -20,16 +25,23 @@ get_latest_release() {
 curl -o- https://raw.githubusercontent.com/creationix/nvm/$(get_latest_release "creationix/nvm")/install.sh | bash
 
 #  Install docker
+sudo apt-get remove docker docker-engine docker.io
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    bionic \
    stable"
 
-
-
 sudo apt-get update
 sudo apt-get install docker-ce
 
-# TODO Install nvm
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Setup MEGASync folders
+mega-sync $HOME/.config/sublime-text-3/Packages/User /SublimeUser
+mega-sync $HOME/Documents /Documents
+mega-sync $HOME/Pictures /Pictures
 
