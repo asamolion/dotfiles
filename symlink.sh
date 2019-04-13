0a61c8a8f4225aa9584116effe94fbf1d639d9fe
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2019-04-14 00:41:00 +0500

    Sun 14 Apr 00:41:00 PKT 2019

--- symlink.sh
+++ symlink.sh
@@ -13,4 +13,6 @@ sudo ln -sf $dotfiles_dir/zshrc ~/.zshrc
 sudo ln -sf $dotfiles_dir/bash_aliases ~/.bash_aliases
 sudo ln -sf $dotfiles_dir/zsh_aliases ~/.zsh_aliases
 sudo ln -sf $dotfiles_dir/terminator_config ~/.config/terminator/config
+sudo ln -sf $dotfiles_dir/neovintageousrc ~/.config/sublime-text-3/Packages/User/.neovintageousrc
+
 
