4c21ff7d6288f9ce159a6d6bdb0aace89a219b1d
Muhammad Osama Arshad <osama@pop-os.localdomain>
2021-12-18 02:53:59 +0500

    Sat Dec 18 02:53:59 AM PKT 2021

--- symlink.sh
+++ symlink.sh
@@ -14,7 +14,7 @@ sudo ln -sf $dotfiles_dir/zshrc ~/.zshrc
 sudo ln -sf $dotfiles_dir/bash_aliases ~/.bash_aliases
 sudo ln -sf $dotfiles_dir/zsh_aliases ~/.zsh_aliases
 sudo ln -sf $dotfiles_dir/terminator_config ~/.config/terminator/config
-sudo ln -sf $dotfiles_dir/neovintageousrc ~/.config/sublime-text-3/Packages/User/.neovintageousrc
+sudo ln -sf $dotfiles_dir/neovintageousrc ~/.config/sublime-text/Packages/User/.neovintageousrc
 sudo ln -sf $dotfiles_dir/init.vim ~/.config/nvim/init.vim
 
 
